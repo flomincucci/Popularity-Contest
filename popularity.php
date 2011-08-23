c2e3b81c1c6d3a05bd5b68b5c50b1a45370fd64a
flomincucci <florencia@mincucci.com.ar>
2011-08-23 17:40:53 -0300

    modified:   popularity.php

--- popularity.php
+++ popularity.php
@@ -24,7 +24,7 @@ function instalacionpoptest() {
 		$sql1 = "CREATE TABLE " . $table_name1 . " (
 			  id int NOT NULL AUTO_INCREMENT,
 			  usuario int NOT NULL,
-			  cantvotos int NOT NULL,
+			  votante int NOT NULL,
 			  UNIQUE KEY id (id)
 			);";
 		$sql2 = "CREATE TABLE " . $table_name2 . " (
@@ -43,5 +43,40 @@ function instalacionpoptest() {
 }
 	 
 register_activation_hook(__FILE__,'instalacionpoptest');
+
+function getvotes($user) {
+	// Retrieves the votes for a user (voted)
+	global $wpdb;
+	$votos = $wpdb->get_var( "SELECT count(1) FROM ".$wpdb->prefix . "poptest_votos where usuario = ".$user." group by usuario");
+	return $votos;
+}
+
+function getvotesavailable($user) {
+	// Retrieves the votes available for a user (voter)
+	global $wpdb;
+	$votos = $wpdb->get_var( "SELECT cantvotos FROM ".$wpdb->prefix . "poptest_votosdisponibles where usuario = ".$user);
+	return $votos;
+}
+
+function vote($voter, $uservoted) {
+ // Action of voting. One logged in user votes a band. The user loses one credit, the band wins a vote. Must check the type of user (bands can't vote, only band can be voted)
+}
+
+function losecredit(int $user, int $quantity) {
+	// The user loses $quantity vote credits. Returns the amount of credits available.
+	global $wpdb;
+	if($wpdb->get_var("SELECT usuario FROM ".$wpdb->prefix . "poptest_votosdisponibles where usuario = ".$user == $user) {
+		// User already exists in the table
+		$votosactuales = getvotesavailable($user);
+		$itsok = $wpdb->update($wpdb->prefix . "poptest_votosdisponibles", array('cantvotos' => $votosactuales - $quantity), array('usuario' => $user));
+		if ($itsok)	$votosdisponibles = $votosactuales - $quantity;
+		else $votosdisponibles = $votosactuales;
+	} else {
+		// Insert user in the table
+		$wpdb->insert($wpdb->prefix . "poptest_votosdisponibles", array('usuario' => $user, 'cantvotos' => 10-$quantity ));
+	}
+	return $votosdisponibles;
+
+}
 
 ?>
