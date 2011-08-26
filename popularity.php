67054a281986cd8151e8d80b5a16af098f6bd612
Flo Mincucci <florencia@mincucci.com.ar>
2011-08-26 00:58:28 -0300

    Fixed issue  with number of votes

--- popularity.php
+++ popularity.php
@@ -47,18 +47,19 @@ register_activation_hook(__FILE__,'instalacionpoptest');
 function getvotes($user) {
 	// Retrieves the votes for a user (voted)
 	global $wpdb;
-	if(user_can($voter, 'banda')) {
+	if(user_can($user, 'banda')) {
 		$votos = $wpdb->get_var( "SELECT count(1) FROM ".$wpdb->prefix . "poptest_votos where usuario = ".$user." group by usuario");
 		return $votos;
 	} else {
 		//Error
+		return false;
 	}
 }
 
 function getvotesavailable($user) {
 	// Retrieves the votes available for a user (voter)
 	global $wpdb;
-	if(user_can($voter, 'integrante')) {
+	if(user_can($user, 'integrante')) {
 		$votos = $wpdb->get_var( "SELECT cantvotos FROM ".$wpdb->prefix . "poptest_votosdisponibles where usuario = ".$user);
 		return $votos;
 	} else {
@@ -69,7 +70,7 @@ function getvotesavailable($user) {
 function getvotados($user) {
 	// Retrieves the bands voted by a user
 	global $wpdb;
-	if(user_can($voter, 'integrante')) {
+	if(user_can($user, 'integrante')) {
 		$votos = $wpdb->get_var( "SELECT count(1) FROM ".$wpdb->prefix . "poptest_votos where votante = ".$user." group by votante");
 		return $votos;
 	} else {
@@ -79,8 +80,8 @@ function getvotados($user) {
 
 function vote($voter, $uservoted) {
  // Action of voting. One logged-in user votes a band. The user loses one credit, the band wins a vote. Must check the type of user (bands can't vote, only band can be voted). Returns false if fails.
-	if(is_user_logged_in() && user_can($voter, 'integrante') && user_van($uservoted, 'banda') {
-		if($wpdb->get_var("SELECT usuario FROM ".$wpdb->prefix . "poptest_votos where votante = ".$voter." and usuario = ".$uservoted) == $voter) {
+	if(is_user_logged_in() && (user_can($voter, 'integrante') || user_can($voter, 'juez') && user_can($uservoted, 'banda') {
+		if(($wpdb->get_var("SELECT usuario FROM ".$wpdb->prefix . "poptest_votos where votante = ".$voter." and usuario = ".$uservoted) == $voter) or ( getvotesavailable($voter) == 0  )) {
 			return false;
 		} else {
 			$wpdb->insert($wpdb->prefix . "poptest_votos", array('usuario' => $uservoted, 'votante' => $voter));
@@ -96,16 +97,19 @@ function vote($voter, $uservoted) {
 function losecredit(int $user, int $quantity) {
 	// The user loses $quantity vote credits. Returns the amount of credits available. User must be 'integrante'
 	global $wpdb;
-	if(user_can($voter, 'integrante')) {
-		if($wpdb->get_var("SELECT usuario FROM ".$wpdb->prefix . "poptest_votosdisponibles where usuario = ".$user == $user) {
+	if(user_can($voter, 'integrante') || user_can($voter,'juez')) {
+		if($wpdb->get_var("SELECT usuario FROM ".$wpdb->prefix . "poptest_votosdisponibles where usuario = ".$user == $user)) {
 			// User already exists in the table
 			$votosactuales = getvotesavailable($user);
+			if (($votosactuales - $quantity) < 0) return false;
 			$itsok = $wpdb->update($wpdb->prefix . "poptest_votosdisponibles", array('cantvotos' => $votosactuales - $quantity), array('usuario' => $user));
 			if ($itsok)	$votosdisponibles = $votosactuales - $quantity;
 			else $votosdisponibles = $votosactuales;
 		} else {
 			// Insert user in the table
-			$votosdisponibles = 10 - $quantity;
+			$maximo = (user_can($voter,'juez'))? 10 : 1;
+			$votosdisponibles = $maximo - $quantity;
+			if ($votosdisponibles < 0) return false;
 			$wpdb->insert($wpdb->prefix . "poptest_votosdisponibles", array('usuario' => $user, 'cantvotos' => $votosdisponible));
 		}
 		return $votosdisponibles;
