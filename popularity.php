a78b51921dbb00a3f7453b3fad9dbfd9f9d25ebd
flomincucci <florencia@mincucci.com.ar>
2011-10-26 16:55:24 -0300

    Simplicity.

--- popularity.php
+++ popularity.php
@@ -13,7 +13,6 @@ function instalacionpoptest() {
    global $wpdb;
 
    $table_name1 = $wpdb->prefix . "poptest_votos";
-	 /*$table_name2 = $wpdb->prefix . "poptest_votosdisponibles";*/
 	 $poptest_db_version = "1.0"; 
 	 if($wpdb->get_var("SHOW TABLES LIKE '$table_name1'") == $table_name1) {
 	 	add_option("poptest_db_version", $poptest_db_version);	
@@ -27,16 +26,9 @@ function instalacionpoptest() {
 			  votante int NOT NULL,
 			  UNIQUE KEY id (id)
 			);";
-		/*$sql2 = "CREATE TABLE " . $table_name2 . " (
-			  id int NOT NULL AUTO_INCREMENT,
-			  usuario int NOT NULL,
-			  cantvotos int NOT NULL,
-			  UNIQUE KEY id (id)
-			);";*/
 		
 		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 		dbDelta($sql1);
-		/*dbDelta($sql2);*/
 		update_option("poptest_db_version", $poptest_db_version);	 	
 	 }
 	 
@@ -47,55 +39,29 @@ register_activation_hook(__FILE__,'instalacionpoptest');
 function getvotes($user) {
 	// Retrieves the votes for a user (voted)
 	global $wpdb;
-	if(user_can($user, 'banda')) {
 		$votos = $wpdb->get_var( "SELECT count(1) FROM ".$wpdb->prefix . "poptest_votos where usuario = ".$user." group by usuario");
+		var_dump($votos);
+		if (!$votos) return 0;
 		return $votos;
-	} else {
-		//Error
-		return false;
-	}
 }
 
 function getvotesavailable($user) {
 	// Retrieves the votes available for a user (voter)
 	global $wpdb;
-	if(user_can($user, 'banda')) {
-		/*$votos = $wpdb->get_var( "SELECT cantvotos FROM ".$wpdb->prefix . "poptest_votosdisponibles where usuario = ".$user);*/
-		$votos = $wpdb->get_var( "SELECT count(1) FROM ".$wpdb->prefix . "poptest_votos where votante = ".$user);
+		$votos = $wpdb->get_var( "SELECT count(1) FROM ".$wpdb->prefix . "poptest_votos where votante = ".$user." group by votante");
 		if ($votos == 1) return 0;
 		if ($votos == false) return 1;
 		else return false;
-	} else {
-		if(user_can($user, 'juez')) {
-		$votos = $wpdb->get_var( "SELECT count(1) FROM ".$wpdb->prefix . "poptest_votos where votante = ".$user);
-		if ($votos <= 10) return 10-$votos;
-		if ($votos == false) return 10;
-		else return false;
-		} else {
-			return false;
-		}
-	}
 }
 
-/*function getvotados($user) {
-	// Retrieves the bands voted by a user
-	global $wpdb;
-	if(user_can($user, 'integrante') || user_can($user, 'juez')) {
-		$votos = $wpdb->get_var( "SELECT count(1) FROM ".$wpdb->prefix . "poptest_votos where votante = ".$user." group by votante");
-		return $votos;
-	} else {
-		return false;
-	}
-}*/
-
 function vote($voter, $uservoted) {
- // Action of voting. One logged-in user votes a band. The user loses one credit, the band wins a vote. Must check the type of user (bands can't vote, only band can be voted). Returns false if fails.
-	if(is_user_logged_in() && user_can($uservoted, 'banda') && (user_can($voter, 'banda') || user_can($voter, 'juez'))) {
-		if(($wpdb->get_var("SELECT usuario FROM ".$wpdb->prefix . "poptest_votos where votante = ".$voter." and usuario = ".$uservoted) == $voter) or ( getvotesavailable($voter) == 0  )) {
+	global $wpdb;
+	if(is_user_logged_in()) {
+		$test = $wpdb->get_var("SELECT usuario FROM ".$wpdb->prefix . "poptest_votos where votante = ".$voter." and usuario = ".$uservoted);
+		if( ($test == $uservoted) || (getvotesavailable($voter) == 0) ) {
 			return false;
 		} else {
 			$wpdb->insert($wpdb->prefix . "poptest_votos", array('usuario' => $uservoted, 'votante' => $voter));
-			/*losecredit($voter,1);*/
 			return true;
 		}
 	} else {
@@ -103,30 +69,5 @@ function vote($voter, $uservoted) {
 		return false;
 	}
 }
-
-/*function losecredit(int $user, int $quantity) {
-	// The user loses $quantity vote credits. Returns the amount of credits available. User must be 'integrante'
-	global $wpdb;
-	if(user_can($voter, 'integrante') || user_can($voter,'juez')) {
-		if($wpdb->get_var("SELECT usuario FROM ".$wpdb->prefix . "poptest_votosdisponibles where usuario = ".$user == $user)) {
-			// User already exists in the table
-			$votosactuales = getvotesavailable($user);
-			if (($votosactuales - $quantity) < 0) return false;
-			$itsok = $wpdb->update($wpdb->prefix . "poptest_votosdisponibles", array('cantvotos' => $votosactuales - $quantity), array('usuario' => $user));
-			if ($itsok)	$votosdisponibles = $votosactuales - $quantity;
-			else $votosdisponibles = $votosactuales;
-		} else {
-			// Insert user in the table
-			$maximo = (user_can($voter,'juez'))? 10 : 1;
-			$votosdisponibles = $maximo - $quantity;
-			if ($votosdisponibles < 0) return false;
-			$wpdb->insert($wpdb->prefix . "poptest_votosdisponibles", array('usuario' => $user, 'cantvotos' => $votosdisponible));
-		}
-		return $votosdisponibles;
-	} else {
-		//Error
-		return false;
-	}
-}*/
 
 ?>
